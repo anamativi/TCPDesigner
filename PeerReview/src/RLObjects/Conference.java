7beab7fdf5ad10e559975992be5b0a8666f27b8b
Matheus Barbieri <mcb_tk@hotmail.com>
2016-06-02 22:37:53 -0300

    updated getLowestSubmittedArticleID

--- PeerReview/src/RLObjects/Conference.java
+++ PeerReview/src/RLObjects/Conference.java
@@ -34,7 +34,7 @@ public class Conference {
         
         Collections.sort(articlesSubmitted);
         
-        return articlesSubmitted[0];
+        return articlesSubmitted.get(0).getID();
     }
 
     /**
