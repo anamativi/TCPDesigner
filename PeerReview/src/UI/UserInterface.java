c0c7fc4341db48af4ba17f1ad4cdd8871388362a
Matheus Barbieri <mcb_tk@hotmail.com>
2016-06-10 04:22:00 -0300

    removed unused imports.

--- PeerReview/src/UI/UserInterface.java
+++ PeerReview/src/UI/UserInterface.java
@@ -7,8 +7,7 @@ import Service.Database;
 import Service.notFoundInDatabase;
 import java.util.*;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 
 public class UserInterface {
