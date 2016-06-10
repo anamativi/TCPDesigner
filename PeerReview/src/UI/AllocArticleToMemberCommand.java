b9c4f6f016ea3fb52f53f32e323c1fa62acfd4b0
lrzg <>
2016-06-10 02:22:14 -0300

    execute

--- PeerReview/src/UI/AllocArticleToMemberCommand.java
+++ PeerReview/src/UI/AllocArticleToMemberCommand.java
@@ -2,29 +2,37 @@ package UI;
 
 
 import UI.Command;
+import Service.Service;
+import Service.notFoundInDatabase;
+import RLObjects.Conference;
 import java.util.*;
 
-/**
- * 
- */
+
 public class AllocArticleToMemberCommand extends Command {
 
-    /**
-     * Default constructor
-     */
+
     public AllocArticleToMemberCommand() {
     }
 
-    /**
-     * @return
-     */
-    public void execute() {
-        // TODO implement here
-        //return null;
+    @Override
+    public void execute(){
+        int nReviewers=0;
+        Conference conference=null;
+        Service service = new Service();
+        UserInterface uInterface = new UserInterface();
+        String conferenceName = uInterface.readString();
+        try{
+            nReviewers = uInterface.readInteger();
+            conference = service.readConference(conferenceName);
+        }catch(InvalidNumberOfReviewersException e){
+            System.out.println(e.toString());
+        }catch(notFoundInDatabase e){
+            System.out.println(e.toString());
+        }
+        
+        service.allocArticlesToMembers(conference,nReviewers,uInterface);
+        
     }
 
-    /**
-     * @return
-     */
 
 }
